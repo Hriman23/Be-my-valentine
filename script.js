de6e2f9ba34b8f4d23eb451d18b5dd989b700355
Artemis23 <hrimanamittal@gmail.com>
2026-02-13 20:04:32 +0530

    UPDATES-

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "pakka beautiful?",
-    "beautiful please...",
-    "Just think about it!",
-    "Don’t say no, I’d be pretty sad.",
-    "I’d be really sad.",
-    "I’d be extremely sad.",
-    "Okay, I won’t ask anymore.",
-    "Haha, kidding! Please say yes 😉"
+    "Are you sureee, Princess?",
+    "Think carefully, Babyyy 👀",
+    "Your Batman is getting nervous...",
+    "My Swara wouldn’t do this… right?",
+    "Okay but like…<br>imagine us on 14th together 🥺",
+    "You know that I’m asking ONLY you<br>and no other girl right?",
+    "Soft no? Or real no? 🤨<br>Don't say no I'd be pretty sad",
+    "Okay, I won’t ask anymore...<br>Haha, just kidding! Please say yes 🥺",
+    "Swara maan jao na...",
+    "Pupu, Batman is sad now :(",
+    "My dearest 29th August girl wouldn't do this to me 😤",
+    "Do you have a crush on me 😭👉🏻👈🏻",
+    "Acha this is the last message pakka-<br>ab you can actually click yes.",
+    "Kya mtlb no?",
+    "Are sacchi! This is the last! Say yes!",
+    "HAWWWW MUJH P SHAQ KAR RHI HO TUM >~<<br>I'm telling you there's no more messages!",
+    "Meri baat hi nhi maanti ye ladkiiii -. -",
+    "Kya mtlb maanti hu?",
+    "Kya mtlb maanti hu? (2.0)",
+    "kya mtlb how do you know what I'm thinking?",
+    "Kya mtlb stop making me blush?",
+    "Okay, I lied. This is the last one, I promise!<br>Please say yes 🥺<br>Will You Be my Valentine my love?"
 ];
 
 let messageIndex = 0;
@@ -16,10 +28,10 @@ let messageIndex = 0;
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
-    noButton.textContent = messages[messageIndex];
+    noButton.innerHTML = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+    yesButton.style.fontSize = `${currentSize * 1.2}px`;
 }
 
 function handleYesClick() {
